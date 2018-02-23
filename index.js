8855aa75ee18352ea64ffaa2ecc4184fb239e419
Shreif Elsaid <shreifelsaid@gmail.com>
2018-02-23 18:59:53 -0500

    file read as list

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("openfile").addEventListener("change", function(){
+
+    var fr = new FileReader();
+    fr.onload = function() {
+    filelines = this.result.split("\n");
+    //document.getElementById("filecontents").textContent = this.result;
+    }
+
+    fr.readAsText(this.files[0]);
+    })
+
