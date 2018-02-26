370683ff2462f71fc2b3609b29f2b0ba2cc9dd5d
arkajitmandal <arkajitmandal@rocheste.edu>
2018-02-26 09:30:43 -0500

    band visualization done

--- band.js
+++ band.js
@@ -36,7 +36,7 @@ function organizeBand(band) {
     }
     
     var j = 0;
-    for( var k =0 ; k < nKpts; k++ ) {
+    for( var k =0 ; k <= nKpts; k++ ) {
         // skipping first line
         j++
         for( var i =0 ; i < nBands; i++ ) {
@@ -46,3 +46,54 @@ function organizeBand(band) {
     }    
     return bands; 
 }
+
+
+function getTicks(){
+    // number of special points
+    nSpts = filelines[2].split(" ")[0];
+    // finding the starting line
+    var start;
+    start = false;
+    // scanning through the entire text
+    for( var i =0 ; i < filelines.length; i++ ) {
+        if (filelines[i].search("Begin band data.") !== -1){
+            start = i ;
+            break
+        }
+    }
+    var end = start;
+    start = start - nSpts ;
+    Kspts = filelines.slice(start,end)
+
+    var pos = []
+    var name = []
+    for (var i = 0 ; i < nSpts; i++ ){
+        // search ith special point position 
+        var count = 0
+        for (var j = 0 ; j < filelines.length; j++ ){
+            if (filelines[j].search("; K point:") !== -1){
+                var point = Kspts[i].split(" ").slice(1,).join(" ");
+                if (filelines[j].search(point) !==-1){
+                    pos.push(count);
+                    name.push(Kspts[i].split(" ")[0]);
+                    break
+                }
+                count++
+            }
+            
+        }
+    }
+    return [name, pos] ; 
+}
+
+function plotBand(bands,xlabel){
+    data = [];
+    for (var i = 0; i< bands.length; i++ ){
+
+        data.push({ x: [...Array(bands[i].length).keys()], y: bands[i],name: 'E <sub>'+ i.toString() + '</sub>'});
+    }
+    var ticks = getTicks();
+    Plotly.newPlot('plot', data, {title:'Band Structure', xaxis:{tickvals:ticks[1],
+        ticktext : ticks[0]                                                  
+    } }); 
+} 
