0ed07b83fc0bf9581053d4358f67f101a7bc65e6
Shreif Elsaid <shreifelsaid@gmail.com>
2018-03-30 23:59:06 -0400

    greatly improved enterdata function, now takes in all the columns of X correctly

--- dos.js
+++ dos.js
@@ -2,20 +2,20 @@ var x =[];
 var y =[];
 
 function create2D(){
-    for (var i =0; i<noOfPoints(); i++){
+    for (var i =0; i<noOfCurves()*(filelines[start()[0]].split(" ").length-1); i++){
         x.push([]);
         y.push([]);
     }
 }
-function noOfPoints(){
-    var noOfPoints = 1;
+function noOfCurves(){
+    var noOfCurves = 1;
     for (var i = 0; i < filelines.length;i++){
         if (filelines[i].includes("# END OF DOS")){
 
-            noOfPoints++;
+            noOfCurves++;
         }
     }
-    return noOfPoints;
+    return noOfCurves;
 }
 function start(){
     var start = [];
@@ -26,10 +26,11 @@ function start(){
             start.push(i+2);
         }
     }
+    
     return start;
 }
 
-function ennnnd(){
+function endOfCurve(){
 
     var end = [];
     for (var i = 0; i < filelines.length;i++){
@@ -69,30 +70,56 @@ function readDos(){
 
 var column; 
 
-function enterData1(){
-    for (var i = 0; i < noOfPoints(); i++){
+
+function enterDataOLD(){
+    for (var i = 0; i < noOfCurves(); i++){
         var a = 0;
-        for(var j = start()[i]; j < ennnnd()[i];j++){
-            x[i][a] = filelines[j].split(" ")[2];
+        // No of columns 
+        var L = filelines[start()[i]].split(" ").length;
+        console.log(L);
+        for(var j = start()[i]; j < endOfCurve()[i];j++){
+            
+            x[i][a] = filelines[j].split(" ")[L-1];
             y[i][a] = filelines[j].split(" ")[0];
             a++;
 
         }
     }
 }
-function enterData(column=0){
-    for (var i = 0; i < noOfPoints(); i++){
-        var a = 0;
-        // No of columns 
-        var L = filelines[start()[i]].split(" ").length;
-        for(var j = start()[i]; j < ennnnd()[i];j++){
-            x[i][a] = filelines[j].split(" ")[L-1];
-            y[i][a] = filelines[j].split(" ")[column];
-            a++;
 
+
+function enterData(){
+    var currentCol = filelines[start()[0]].split(" ").length-1;
+    var m = 0;
+    var shiftCol = 0;
+    //4 is no of curves
+    for (i = 0; i < noOfCurves()*(filelines[start()[0]].split(" ").length-1); i++){
+       
+        var a = 0;
+        for(var j = start()[m]; j < endOfCurve()[m];j++){
+            x[i][a] = filelines[j].split(" ")[currentCol-shiftCol];
+            y[i][a] = filelines[j].split(" ")[0];
+
+            a++;
         }
+        if (shiftCol ==(currentCol-1)){
+            shiftCol = 0;
+        }
+        else {
+            shiftCol++;
+        }
+        
+        if ((m+1)%currentCol == 0){
+            m=0;
+        }
+
+        m++;
+
     }
+
 }
+
+
 
 
 function plotDos(){
@@ -102,4 +129,5 @@ function plotDos(){
         type: 'scatter'
       };
     Plotly.newPlot('plot',[plt1]);
+    
 }
